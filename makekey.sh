7a05ab8cafbc5b7c471fda8de4dc45d4ac74e318
黄正 <zheng.huang@flaginfo.com.cn>
2018-06-24 01:25:16 +0800

    uip

--- makekey.sh
+++ makekey.sh
@@ -4,14 +4,14 @@
 #touch ./demoCA/index.txt
 #echo 01 > ./demoCA/serial
 
-
+mkdir output
 
  #创建自己的CA机构
 #为CA生成私钥
 openssl genrsa -out output/ca-key.pem -des 2048
 
 #通过CA私钥生成CSR
-openssl req -new -days 122 -key output/ca-key.pem -config ./_cfg/openssl.cnf -out output/ca-csr.pem
+openssl req -new -days 122 -key output/ca-key.pem -config _cfg/openssl.cnf -out output/ca-csr.pem
 
 #通过CSR文件和私钥生成CA证书
 openssl x509 -req -days 122 -in output/ca-csr.pem -signkey output/ca-key.pem -out output/ca-cert.pem
@@ -23,10 +23,10 @@ openssl x509 -req -days 122 -in output/ca-csr.pem -signkey output/ca-key.pem -ou
 openssl genrsa -out output/server-key.pem 2048
 
 #利用服务器私钥文件服务器生成CSR
-openssl req -new -days 122 -key output/server-key.pem -config ./_cfg/openssl.cnf -out output/server-csr.pem
+openssl req -new -days 122 -key output/server-key.pem -config _cfg/openssl.cnf -out output/server-csr.pem
 
 #通过服务器私钥文件和CSR文件生成服务器证书
-openssl x509 -req -days 122 -CA output/ca-cert.pem -CAkey output/ca-key.pem -CAcreateserial -in output/server-csr.pem -out output/server-cert.pem -extensions v3_req -extfile ./_cfg/openssl.cnf
+openssl x509 -req -days 122 -CA output/ca-cert.pem -CAkey output/ca-key.pem -CAcreateserial -in output/server-csr.pem -out output/server-cert.pem -extensions v3_req -extfile _cfg/openssl.cnf
  
  
  #创建客户端证书
@@ -35,8 +35,8 @@ openssl x509 -req -days 122 -CA output/ca-cert.pem -CAkey output/ca-key.pem -CAc
 openssl genrsa -out output/client-key.pem 2048
 
 #利用私钥生成CSR
-openssl req -days 122 -new -key output/client-key.pem -config ./_cfg/openssl.cnf -out output/./output/client-csr.pem
+openssl req -days 122 -new -key output/client-key.pem -config _cfg/openssl.cnf -out output/client-csr.pem
 
 #生成客户端证书
-openssl x509 -req -days 122 -CA output/ca-cert.pem -CAkey output/ca-key.pem -CAcreateserial -in output/client-csr.pem -out output/./output/client-cert.pem
+openssl x509 -req -days 122 -CA output/ca-cert.pem -CAkey output/ca-key.pem -CAcreateserial -in output/client-csr.pem -out output/client-cert.pem
 

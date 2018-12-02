de0609c0f772a043a892c7766ac358d0b4b6f1c0
hzsrc <hz932@qq.com>
2018-12-02 18:30:55 +0800

    add

--- makekey.sh
+++ makekey.sh
@@ -22,15 +22,23 @@ openssl req -batch -passin "pass:${pwd}" -new -days 122 -key output/ca-key.pem -
 #通过CSR文件和私钥生成CA证书
 openssl x509 -passin "pass:${pwd}" -req -days 122 -in output/ca-csr.pem -signkey output/ca-key.pem -out output/ca-cert.pem
 
+
+
+
 #--创建服务器端证书
 #为服务器生成私钥
-openssl genrsa -passout "pass:${pwd}" -out output/server-key.pem -des 2048
+openssl genrsa -passout "pass:${pwd}" -out output/server-key.pem 2048
 
 #利用服务器私钥文件服务器生成CSR
 openssl req -batch -passin "pass:${pwd}" -new -days 122 -key output/server-key.pem -config _cfg/openssl.cnf -out output/server-csr.pem
 
 #通过服务器私钥文件和CSR文件生成服务器证书
 openssl x509 -passin "pass:${pwd}" -req -days 122 -CA output/ca-cert.pem -CAkey output/ca-key.pem -CAcreateserial -in output/server-csr.pem -out output/server-cert.pem -extensions v3_req -extfile _cfg/openssl.cnf
+
+
+
+
+
 
 
 #--创建客户端证书
